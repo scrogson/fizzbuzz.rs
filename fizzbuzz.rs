223ddd8c520bce1f3a0624743e0c7650f6231f8b
Sonny Scroggin <scrogson@gmail.com>
2014-10-10 18:12:44 -0500

    Clean up with match

--- fizzbuzz.rs
+++ fizzbuzz.rs
@@ -1,40 +1,25 @@
-fn div_by_three(num: int) -> bool {
-    num % 3 == 0
-}
-
-fn div_by_five(num: int) -> bool {
-    num % 5 == 0
-}
-
-fn div_by_fifteen(num: int) -> bool {
-    num % 15 == 0
+fn div_by(num: int, div: int) -> bool {
+    num % div == 0
 }
 
 fn main() {
-    for num in range(1i, 101) {
+    for num in range(1, 101) {
         println!("{:s}",
-            if div_by_fifteen(num) { "FizzBuzz".to_string() }
-            else if div_by_five(num) { "Buzz".to_string() }
-            else if div_by_three(num) { "Fizz".to_string() }
-            else { num.to_string() }
+           match (div_by(num, 3), div_by(num, 5)) {
+               (true, true)   => "FizzBuzz".to_string(),
+               (true, false)  => "Fizz".to_string(),
+               (false, true)  => "Buzz".to_string(),
+               (false, false) => num.to_string()
+           }
         );
     }
 }
 
 #[test]
-fn test_div_by_three() {
-    assert!(!div_by_three(1))
-    assert!(div_by_three(3))
+fn test_div_by() {
+    assert!(!div_by(1, 3))
+    assert!(div_by(3, 3))
+    assert!(!div_by(1, 5))
+    assert!(div_by(5, 5))
 }
 
-#[test]
-fn test_div_by_five() {
-    assert!(!div_by_five(1))
-    assert!(div_by_five(5))
-}
-
-#[test]
-fn test_div_by_fifteen() {
-    assert!(!div_by_fifteen(1))
-    assert!(div_by_fifteen(15))
-}
